cc04c7c5b73d4a31c15e1e01b31c52eddbbe92fe
matias <mchialva@mail.austral.edu.ar>
2024-04-08 11:42:32 -0300

    implementing mqtt connection

--- frontend/src/components/start/Start.jsx
+++ frontend/src/components/start/Start.jsx
@@ -1,16 +1,51 @@
 import React from 'react';
 import './Start.css';
 import { useNavigate } from 'react-router-dom';
+import mqtt from 'mqtt';
 
 function Start() {
     let navigate = useNavigate();
 
+    // MQTT connection options
+    const options = {
+        connectTimeout: 4000, // Timeout period
+        // Authentication (if needed)
+        //clientId: 'react_mqtt_client',
+        // If your MQTT broker requires authentication, uncomment these lines
+        // username: 'your_username',
+        // password: 'your_password',
+    };
+
+
+    // Function to publish a message to the MQTT broker
+    const publishMessage = () => {
+        const client = mqtt.connect('ws://34.201.59.202:1883', options);
+
+        client.on('connect', () => {
+            console.log('Connected to MQTT Broker on EC2');
+
+            // Publish a message to a topic
+            client.publish('start/button', 'Button clicked', {}, (error) => {
+                if (error) {
+                    console.error('Publish error:', error);
+                }
+            });
+
+            // After publishing the message, you can navigate to the home page
+            navigate('/home');
+
+            // Optionally, end the connection when done
+            client.end();
+        });
+    };
+
     return (
         <div className="start">
             <h1>Welcome to the App</h1>
-            <button onClick={() => navigate('/home')}>Start</button>
+            <button onClick={publishMessage}>Start</button>
         </div>
     );
 }
 
 export default Start;
+
